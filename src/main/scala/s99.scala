4d5305517557af58c01df9494ee9a3be545a63b3
shotanue <qwerty5150goodmorning@gmail.com>
2018-04-15 23:29:52 +0900

    コップ本を読みだした。

--- src/main/scala/s99.scala
+++ src/main/scala/s99.scala
@@ -3,20 +3,25 @@ class s99 {
   //P01
   //写経
   //最後の項目に当たったらhが返るような実装
-    def last[A](list:List[A]): A = list match {
-  //    先頭のみ
-      case h :: Nil => h
-      //    _::tailで先頭とそれ以外に分離する。
-      //     これを繰り返して先頭を１個ずつ減らしていくと、先頭１個だけの状態に行き着く
-      case _ :: tail => last(tail)
-      case _ => throw new NoSuchElementException
-    }
-
+  def last[A](list: List[A]): A = list match {
+    //    先頭のみ
+    case h :: Nil => h
+    //    _::tailで先頭とそれ以外に分離する。
+    //     これを繰り返して先頭を１個ずつ減らしていくと、先頭１個だけの状態に行き着く
+    case _ :: tail => last(tail)
+    case _ => throw new NoSuchElementException
+  }
   //P01
   //写経をいじって順番を入れ替えるとうごかない。
-//  def last[A](list:List[A]): A = list match {
-//    case _ :: tail => last(tail)
-//    case h :: Nil => h
-//    case _ => throw new NoSuchElementException
-//  }
+  //  def last[A](list:List[A]): A = list match {
+  //    case _ :: tail => last(tail)
+  //    case h :: Nil => h
+  //    case _ => throw new NoSuchElementException
+  //  }
+
+  //reduceで実装してみる
+  def count(xs: List[Int]): Int = {
+    if (xs.isEmpty) 0 else
+      xs.reduce((curry, _) => curry + 1)
+  }
 }
--- src/test/scala/s99Spec.scala
+++ src/test/scala/s99Spec.scala
@@ -2,12 +2,30 @@
 import org.specs2.mutable.Specification
 
 class s99Spec extends Specification {
+  val s99 = new s99()
+
   "s99" should {
     "tmp" in {
-      val s99 = new s99()
       val tmp = List(1, 2, 3)
       println(s99.last(tmp))
       s99.last(tmp) must_== 3
+    }
+  }
+  "Listのカウント" >> {
+    "空は0を返す" >> {
+      s99.count(List()) must_== 0
+    }
+    "要素の数の値を返す" >> {
+      val oneItem = List(1)
+      val twoItems = List(1, 1)
+      val threeItems = List(1, 1, 1)
+      s99.count(oneItem) must_== 1
+      s99.count(twoItems) must_== 2
+      s99.count(threeItems) must_== 3
+    }
+    "最後の値が戻り値ではない" >> {
+      val fourItems = List(1, 2, 3, 9999)
+      s99.count(fourItems) must_== 4
     }
   }
 }
