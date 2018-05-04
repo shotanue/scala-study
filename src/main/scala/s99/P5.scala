92a06c486ff086044397785d20c15d3ebbd9c5d9
shotanue <qwerty5150goodmorning@gmail.com>
2018-05-04 23:38:39 +0900

    P5を写経 matchよりfoldLeftの実装がなんとなくわかりやすい印象

--- src/main/scala/s99/P5.scala
+++ src/main/scala/s99/P5.scala
@@ -1,7 +1,18 @@
 package s99
 
 object P5 {
-//  def reverse [A](xs:List[A]):List[A] = xs match {
-//    case head::body::tail => reverse(head += body)
-//  }
+  def reverse[A](ls: List[A]): List[A] = {
+    //1,2,3,4 => op(op(op(op(empty, 1),2),3),4)みたいな感じかな
+    //受け取った要素を引き継いだ要素の先頭に加えると逆順になるよねという
+    ls.foldLeft(List[A]()) { (r, h) => h :: r }
+  }
+
+  // Tail recursive.
+  def reverseTailRecursive[A](ls: List[A]): List[A] = {
+    def reverseR(result: List[A], curList: List[A]): List[A] = curList match {
+      case Nil       => result
+      case h :: tail => reverseR(h :: result, tail)
+    }
+    reverseR(Nil, ls)
+  }
 }
